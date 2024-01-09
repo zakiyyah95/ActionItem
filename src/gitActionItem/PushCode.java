d8db7a44064e2e1096cb9b655857a637ffd65754
zakiyyah95 <zakiyyah.be@gmail.com>
2024-01-09 11:16:18 -0500

    Pushing action item into github

---
+++ src/gitActionItem/PushCode.java
@@ -0,0 +1,9 @@
+package gitActionItem;
+
+public class PushCode {
+
+	public void practiceGit() {
+		System.out.println("This is an action item. The directions are to push this code into github.");
+	}
+	
+}
